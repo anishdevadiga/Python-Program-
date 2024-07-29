212765b21e3a2ff02da52a979faf242457c1105c
Anish Sherigar <143006940+anishdevadiga@users.noreply.github.com>
2024-07-29 22:15:02 +0530

    Delete lab-week1 directory

--- lab-week1/qn1.py
+++
@@ -1,6 +0,0 @@
-length = float(input('Enter the length:'))
-width = float(input('Enter the breadth:'))
-
-area_of_field = (length*width)/43560
-
-print("Area of the field in acres :",area_of_field)
--- lab-week1/qn3.py
+++
@@ -1,3 +0,0 @@
-import time
-current_time=time.asctime()
-print("The Current time and Date is : ",current_time)
